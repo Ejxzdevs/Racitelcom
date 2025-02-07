89dbe796057d2ee96b304a09e438ca17083de9c8
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-02-07 08:11:06 +0800

    add update form

--- src/view/schedule.php
+++ src/view/schedule.php
@@ -39,7 +39,7 @@ $data = $getData->getAll();
                 <input type="text" name="id" value="<?php echo htmlspecialchars( $display['schedule_id'] );?>" hidden>
                 <button class="text-blue-500 hover:text-blue-700 focus:outline-none">Delete</button>
               </form>
-                <button class="text-blue-500 hover:text-blue-700 focus:outline-none">Edit</button>
+              <a href="javascript:void(0);" class="edit-link" data-schedule="<?php echo htmlspecialchars(json_encode($data)); ?>">edit</a>
             </td>
         </tr>
         <?php endforeach; ?>
@@ -50,6 +50,7 @@ $data = $getData->getAll();
 
 
 <!-- POP UP -->
+ <!-- ADD FORM -->
 <div id="modal" class="fixed inset-0 bg-opacity-50 flex justify-center items-center hidden" style="background-color: rgba(0, 0, 0, 0.5);">
   <div class="bg-white p-6 rounded-lg w-96 max-h-[80vh] overflow-y-auto">
     <h2 class="text-2xl mb-4">New Schedule</h2>
@@ -81,4 +82,63 @@ $data = $getData->getAll();
     </form>
   </div>
 </div>
+
+
+<!-- EDIT -->
+
+<div id="editmodal" class="fixed inset-0 bg-opacity-50 flex justify-center items-center hidden" style="background-color: rgba(0, 0, 0, 0.5);">
+  <div class="bg-white p-6 rounded-lg w-96 max-h-[80vh] overflow-y-auto">
+    <h2 class="text-2xl mb-4">Edit Schedule</h2>
+    <form id="popupForm" action="../controller/scheduleController.php" method="POST">
+      <!-- Input Field -->
+      <div class="mb-4">
+        <input type="hidden" name="csrf_token" value="<?php echo $csrf_token ?>">
+        <input type="hidden" name="update">
+        <label for="schedule_name" class="block text-sm font-medium text-gray-700">Schedule Name</label>
+        <input type="text" id="edit_schedule_name" name="schedule_name" class="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter Schedule Name:" required>
+      </div>
+      <div class="mb-4">
+        <label for="time_start" class="block text-sm font-medium text-gray-700">Time Start</label>
+        <input type="time" id="edit_time_start" name="time_start" class="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+      </div>
+      <div class="mb-4">
+        <label for="time_end" class="block text-sm font-medium text-gray-700">Time End</label>
+        <input type="time" id="edit_time_end" name="time_end" class="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+      </div>
+      <!-- Submit Button -->
+      <div class="flex justify-between items-center">
+        <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">
+          Submit
+        </button>
+        <button type="button" id="closeEditModal" class="cursor-pointer bg-red-500 text-white py-2 px-4 rounded hover:bg-red-700">
+          Close
+        </button>
+      </div>
+    </form>
+  </div>
+</div>
 <script src="../assets/js/popUpform.js"></script>
+<script>
+    const editModal = document.getElementById("editmodal");
+    document.querySelectorAll('.edit-link').forEach(link => {
+        link.addEventListener('click', function(event) {
+            const getData = JSON.parse(event.target.getAttribute('data-schedule'));
+            const data = getData[0];
+
+            document.getElementById('edit_schedule_name').value = data.schedule_id;
+            document.getElementById('edit_time_start').value = data.time_start;
+            document.getElementById('edit_time_end').value = data.time_end;
+    
+            editModal.classList.remove("hidden");
+            
+            console.log(data);
+        });
+    });
+
+    const closeEditModal = document.getElementById("closeEditModal");
+    
+    closeEditModal.addEventListener("click", function() {
+        editModal.classList.add("hidden");
+    });
+</script>
+

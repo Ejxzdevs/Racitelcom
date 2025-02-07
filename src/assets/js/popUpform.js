b7502a5a564f7633d346b9caf6687eb060a0d7c5
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-02-07 08:12:46 +0800

    add event listener for edit modal

--- src/assets/js/popUpform.js
+++ src/assets/js/popUpform.js
@@ -11,8 +11,25 @@ closeModal.addEventListener("click", function() {
   modal.classList.add("hidden");
 });
 
-// popupForm.addEventListener("submit", function(event) {
-//   event.preventDefault(); 
-//   alert("Form Submitted!");
-//   modal.classList.add("hidden");
-// });
+// EDIT FORM
+const editModal = document.getElementById("editmodal");
+    document.querySelectorAll('.edit-link').forEach(link => {
+        link.addEventListener('click', function(event) {
+            const getData = JSON.parse(event.target.getAttribute('data-schedule'));
+            const data = getData[0];
+
+            document.getElementById('edit_schedule_name').value = data.schedule_id;
+            document.getElementById('edit_time_start').value = data.time_start;
+            document.getElementById('edit_time_end').value = data.time_end;
+    
+            editModal.classList.remove("hidden");
+            
+            console.log(data);
+        });
+    });
+
+    const closeEditModal = document.getElementById("closeEditModal");
+    
+    closeEditModal.addEventListener("click", function() {
+        editModal.classList.add("hidden");
+    });

4ebf32479b5192a35104eefc6e877160628d167f
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-02-08 01:34:25 +0800

    create query for delete

--- src/model/departmentModel.php
+++ src/model/departmentModel.php
@@ -51,7 +51,7 @@ class DepartmentModel extends Database {
     public function delete($id){
         $connection = parent::openConnection();
         try {
-            $stmt = $connection->prepare("DELETE FROM Schedules WHERE schedule_id = ? ");
+            $stmt = $connection->prepare("DELETE FROM departments WHERE department_id = ? ");
             $stmt->bindParam(1, $id, PDO::PARAM_INT);
             $stmt->execute();
             return 200;

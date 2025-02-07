6e4897127c5d08707ca928bcc29bceceb7306d7b
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-02-07 23:34:45 +0800

    fixed bug

--- src/view/schedule.php
+++ src/view/schedule.php
@@ -5,41 +5,46 @@ $csrf_token = CsrfHelper::generateToken();
 $getData = new ScheduleApi();
 $data = $getData->getAll();
 ?>
-<div class="container flex flex-col"  style="padding: 0 2rem;">
+<div class="container flex flex-col gap-2 text-[14px]"  style="padding: 0 2rem;">
     <div class="h-[3rem] flex items-center justify-between ">
-        <label class="text-gray-800 text-[1.5rem] font-bold font-inter">
+        <label class="text-gray-800 text-[1.3rem] font-bold">
             Schedule List
         </label>
-        <button id="openModal" class="bg-blue-500 text-white hover:bg-blue-700 rounded-sm" style="padding: 2px 4px;">
-            Add Schedule
+        <button id="openModal" class="py-[5px] px-2 border-1 border-gray-300 text-blue-500 hover:text-blue-700 cursor-pointer shadow-md rounded-sm">
+            <i class="fas fa-plus fa"></i>
+            Schedule
         </button>
     </div>
     <div class="overflow-x-auto" >
-      <table class="min-w-full table-auto rounded-lg bg-white shadow-md overflow-hidden">
+      <table class="w-full table-fixed rounded-lg bg-white shadow-md overflow-hidden">
         <thead class="bg-gray-200">
           <tr class="text-gray-700 border-b">
-            <th class="px-4 py-2 text-left">Id</th>
-            <th class="px-4 py-2 text-left">Name</th>
-            <th class="px-4 py-2 text-left">Time Start</th>
-            <th class="px-4 py-2 text-left">Time End</th>
-            <th class="px-4 py-2 text-left">Action</th>
+            <th class="py-2 text-center">Id</th>
+            <th class="py-2 text-center">Name</th>
+            <th class="py-2 text-center">Time Start</th>
+            <th class="py-2 text-center">Time End</th>
+            <th class="py-2 text-center">Action</th>
           </tr>
         </thead>
       <tbody>
         <?php foreach ($data as $display): ?>
         <tr class="border-b hover:bg-gray-50 transition-colors">
-            <td class="px-4 py-2"><?php echo htmlspecialchars($display['schedule_id']); ?></td>
-            <td class="px-4 py-2"><?php echo htmlspecialchars($display['schedule_name']); ?></td>
-            <td class="px-4 py-2"><?php echo htmlspecialchars((new DateTime($display['time_start']))->format('g:i A')); ?></td>
-            <td class="px-4 py-2"><?php echo htmlspecialchars((new DateTime($display['time_end']))->format('g:i A')); ?></td>
-            <td class="px-4 py-2">
+            <td class="py-2 text-center"><?php echo htmlspecialchars($display['schedule_id']); ?></td>
+            <td class="py-2 text-center"><?php echo htmlspecialchars($display['schedule_name']); ?></td>
+            <td class="py-2 text-center"><?php echo htmlspecialchars((new DateTime($display['time_start']))->format('g:i A')); ?></td>
+            <td class="py-2 text-center"><?php echo htmlspecialchars((new DateTime($display['time_end']))->format('g:i A')); ?></td>
+            <td class="py-2 flex flex-row justify-center gap-4">
               <form action="../controller/scheduleController.php" method="POST">
                 <input type="hidden" name="csrf_token" value="<?php echo $csrf_token ?>">
                 <input type="hidden" name="delete">
                 <input type="text" name="id" value="<?php echo htmlspecialchars( $display['schedule_id'] );?>" hidden>
-                <button class="text-blue-500 hover:text-blue-700 focus:outline-none">Delete</button>
+                <button class="text-red-500 hover:text-red-700 focus:outline-none">
+                  <i class="fas fa-trash"></i>
+                </button>
               </form>
-              <a href="javascript:void(0);" class="edit-link" data-schedule="<?php echo htmlspecialchars(json_encode($data)); ?>">edit</a>
+              <a href="javascript:void(0);" class="edit-link text-blue-500 hover:text-blue-700"  data='<?php echo htmlspecialchars(json_encode($display), ENT_QUOTES, 'UTF-8'); ?>'>
+                <i class="fas fa-edit"></i>
+              </a>
             </td>
         </tr>
         <?php endforeach; ?>
@@ -53,13 +58,13 @@ $data = $getData->getAll();
  <!-- ADD FORM -->
 <div id="modal" class="fixed inset-0 bg-opacity-50 flex justify-center items-center hidden" style="background-color: rgba(0, 0, 0, 0.5);">
   <div class="bg-white p-6 rounded-lg w-96 max-h-[80vh] overflow-y-auto">
-    <h2 class="text-2xl mb-4">New Schedule</h2>
-    <form id="popupForm" action="../controller/scheduleController.php" method="POST">
+    <h2 class="text-[20px] mb-4">New Schedule</h2>
+    <form id="addForm" action="../controller/scheduleController.php" method="POST">
       <!-- Input Field -->
       <div class="mb-4">
         <input type="hidden" name="csrf_token" value="<?php echo $csrf_token ?>">
         <input type="hidden" name="insert">
-        <label for="schedule_name" class="block text-sm font-medium text-gray-700">Schedule Name</label>
+        <label for="schedule_name" class="block text-[14px]  font-medium text-gray-700">Schedule Name</label>
         <input type="text" id="schedule_name" name="schedule_name" class="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter Schedule Name:" required>
       </div>
       <div class="mb-4">
@@ -72,10 +77,10 @@ $data = $getData->getAll();
       </div>
       <!-- Submit Button -->
       <div class="flex justify-between items-center">
-        <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">
+        <button type="submit" class="py-[5px] px-4 border-1 border-gray-300 text-blue-500 hover:text-blue-700 cursor-pointer shadow-md rounded-sm">
           Submit
         </button>
-        <button type="button" id="closeModal" class="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-700">
+        <button type="button" id="closeModal" class="py-[5px] px-4 border-1 border-gray-300 text-red-500 hover:text-red-700 cursor-pointer shadow-md rounded-sm">
           Close
         </button>
       </div>
@@ -88,8 +93,8 @@ $data = $getData->getAll();
 
 <div id="editmodal" class="fixed inset-0 bg-opacity-50 flex justify-center items-center hidden" style="background-color: rgba(0, 0, 0, 0.5);">
   <div class="bg-white p-6 rounded-lg w-96 max-h-[80vh] overflow-y-auto">
-    <h2 class="text-2xl mb-4">Edit Schedule</h2>
-    <form id="popupForm" action="../controller/scheduleController.php" method="POST">
+    <h2 class="text-[20px] mb-4">Update Schedule</h2>
+    <form id="editForm" action="../controller/scheduleController.php" method="POST">
       <!-- Input Field -->
       <div class="mb-4">
         <input type="hidden" name="csrf_token" value="<?php echo $csrf_token ?>">
@@ -108,10 +113,10 @@ $data = $getData->getAll();
       </div>
       <!-- Submit Button -->
       <div class="flex justify-between items-center">
-        <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">
+        <button type="submit" class="py-[5px] px-4 border-1 border-gray-300 text-blue-500 hover:text-blue-700 cursor-pointer shadow-md rounded-sm">
           Submit
         </button>
-        <button type="button" id="closeEditModal" class="cursor-pointer bg-red-500 text-white py-2 px-4 rounded hover:bg-red-700">
+        <button type="button" id="closeEditModal" class="py-[5px] px-4 border-1 border-gray-300 text-red-500 hover:text-red-700 cursor-pointer shadow-md rounded-sm">
           Close
         </button>
       </div>

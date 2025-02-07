1be5364f36f810a182e7b3bb27dc76c63af37a52
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-02-07 08:39:08 +0800

    rename

---
+++ src/assets/js/schedule.js
@@ -0,0 +1,36 @@
+const openModal = document.getElementById("openModal");
+const closeModal = document.getElementById("closeModal");
+const modal = document.getElementById("modal");
+const popupForm = document.getElementById("popupForm");
+
+openModal.addEventListener("click", function() {
+  modal.classList.remove("hidden");
+});
+
+closeModal.addEventListener("click", function() {
+  modal.classList.add("hidden");
+});
+
+// EDIT FORM
+const editModal = document.getElementById("editmodal");
+    document.querySelectorAll('.edit-link').forEach(link => {
+        link.addEventListener('click', function(event) {
+            const getData = JSON.parse(event.target.getAttribute('data-schedule'));
+            const data = getData[0];
+
+            document.getElementById('edit_schedule_id').value = data.schedule_id;
+            document.getElementById('edit_schedule_name').value = data.schedule_name;
+            document.getElementById('edit_time_start').value = data.time_start;
+            document.getElementById('edit_time_end').value = data.time_end;
+    
+            editModal.classList.remove("hidden");
+            
+            console.log(data);
+        });
+    });
+
+    const closeEditModal = document.getElementById("closeEditModal");
+    
+    closeEditModal.addEventListener("click", function() {
+        editModal.classList.add("hidden");
+    });

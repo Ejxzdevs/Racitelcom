c4620c0655e01ff2b19b9b03d3fd22f05d64b729
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-02-12 17:46:12 +0800

    assign parameters for conn from .ENV

--- config/database.php
+++ config/database.php
@@ -1,13 +1,15 @@
 <?php 
-
+require_once '../../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '../../');
+$dotenv->load();
 class Database {
-    private $host = 'localhost';
-    private $username = 'root';
-    private $password = '';
-    private $dbname = 'payroll';
+    private $host,$username,$password,$dbname;
     protected $conn;
     public function __construct() {
-    
+        $this->host = $_ENV['DB_HOST']; 
+        $this->username = $_ENV['DB_USERNAME']; 
+        $this->password = $_ENV['DB_PASSWORD']; 
+        $this->dbname = $_ENV['DB_NAME']; 
         try {
             $this->conn = new PDO("mysql:host=$this->host", $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -15,8 +17,6 @@ class Database {
             // Create the database if it doesn't exist
             $create_database = "CREATE DATABASE IF NOT EXISTS $this->dbname";
             $this->conn->exec($create_database);
-
-            // Select the database for future operations
             $this->conn->exec("USE $this->dbname");
         } catch (PDOException $e) {
             echo "Connection failed: " . $e->getMessage();
@@ -27,7 +27,6 @@ class Database {
     protected function OpenConnection() {
         return $this->conn;
     }
-
     // CLOSE CONNECTION
     protected function CloseConnection() {
         echo '<script>console.log("connection close");</script>';

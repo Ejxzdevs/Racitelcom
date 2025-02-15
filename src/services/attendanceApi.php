592d98a6aeef750db6e184a77faf4b83869f9cee
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-02-15 21:54:41 +0800

    create api for attendance

--- src/services/attendanceApi.php
+++ src/services/attendanceApi.php
@@ -6,7 +6,9 @@ class AttendanceApi extends Database {
     public function getAll() {
         $connection = parent::openConnection();
         try {
-            $stmt = $connection->prepare("SELECT * FROM departments");
+            $stmt = $connection->prepare("SELECT * FROM attendances 
+            inner join employees on attendances.employee_id = employees.employee_id
+            where attendances.is_deleted = 0");
             $stmt->execute();
             $data = $stmt->fetchAll(PDO::FETCH_ASSOC); 
             return $data;

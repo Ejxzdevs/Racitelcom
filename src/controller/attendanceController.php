f662ba1d3b33695ea0b7368ada7610b8b557f630
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-02-15 21:53:24 +0800

    comment update method

---
+++ src/controller/attendanceController.php
@@ -0,0 +1,65 @@
+<?php
+require_once '../helper/csrf.php';
+require_once '../helper/sanitizeInput.php';
+require_once '../model/attendanceModel.php';
+
+class AttendanceController extends SanitizeInput { 
+    private $model;
+    public function __construct(AttendanceModel $model) {
+        $this->model = $model;
+    }
+    public function create($data){
+        $dataSanitized = $this->sanitizeInput($data);
+        return $this->model->insert($dataSanitized);
+    }
+
+    // public function update($data){
+    //     $dataSanitized = $this->sanitizeInput($data);
+    //     return $this->model->update($dataSanitized);
+    // }
+
+    public function delete($id){
+        $idSanitized = filter_var($id, FILTER_VALIDATE_INT);
+        return $this->model->delete(   $idSanitized);
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if (!isset($_POST['csrf_token']) || !CsrfHelper::validateToken($_POST['csrf_token'])) {
+        http_response_code(403);
+        echo "<script>alert('Invalid CSRF token!')</script>";
+        echo "<script>window.location.href='../view/main.php?route=attendance'</script>";
+        exit();
+    }
+    CsrfHelper::regenerateToken();
+    $Model = new AttendanceModel();
+    $attendance = new AttendanceController($Model);
+
+    try {
+        if (isset($_POST['insert'])) {
+            $status = $attendance->create($_POST);
+            echo $status === 200 
+                ? "<script>alert('Data Added Successfully')</script><script>window.location.href='../view/main.php?route=attendance'</script>"
+                : "<script>alert('Data insertion failed')</script><script>window.location.href='../view/main.php?route=attendance'</script>";
+        }
+
+        if (isset($_POST['update'])) {
+            
+           $status = $attendance->update($_POST);
+           echo $status === 200 
+           ? "<script>alert('Data Updated Successfully')</script><script>window.location.href='../view/main.php?route=attendance'</script>"
+           : "<script>alert('Data updating failed')</script><script>window.location.href='../view/main.php?route=attendance'</script>";
+        }
+
+        if (isset($_POST['delete'])) {
+            $status = $attendance->delete($_POST['id']);
+            echo $status === 200 
+                ? "<script>alert('Data deleted Successfully')</script><script>window.location.href='../view/main.php?route=attendance'</script>"
+                : "<script>alert('Data deleted failed')</script><script>window.location.href='../view/main.php?route=attendance'</script>";
+            
+        }
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage();
+    }
+
+}

9fd2963620f5e93e4ccbe121449d9a09873ee77f
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-02-18 23:24:37 +0800

    init

---
+++ src/model/deductionModel.php
@@ -0,0 +1,66 @@
+<?php
+require_once '../../config/database.php';
+
+class DeductionModel extends Database {
+
+    public function insert($data){
+        $connection = parent::openConnection();
+        try {
+            $connection->beginTransaction();
+            $stmt = $connection->prepare("INSERT INTO deductions 
+                        (deduction_name,deduction_rate) 
+                        VALUES (?,?)");
+            $stmt->bindParam(1, $data['deduction_name']);
+            $stmt->bindParam(2, $data['deduction_rate']);
+            $stmt->execute();
+            $connection->commit();
+            return 200;
+        } catch (PDOException $e) {
+            $connection->rollBack();
+            echo "Error: " . $e->getMessage();
+            return false;
+        } finally {
+            parent::closeConnection();
+        }
+    }
+
+    
+    public function update($data) {
+        $connection = parent::openConnection();
+        try {
+            $connection->beginTransaction();
+            $stmt = $connection->prepare("UPDATE deductions  
+                            SET deduction_name = ? ,  deduction_rate = ? , deduction_status = ?
+                            WHERE deduction_id = ?");
+            $stmt->bindParam(1, $data['deduction_name']);
+            $stmt->bindParam(2, $data['deduction_rate']);
+            $stmt->bindParam(3, $data['deduction_status']);
+            $stmt->bindParam(4, $data['deduction_id'], PDO::PARAM_INT);
+            $stmt->execute();
+            $connection->commit();
+            return 200;
+        } catch (PDOException $e) {
+            $connection->rollBack();
+            echo "Error: " . $e->getMessage();
+            return false;
+        } finally {
+            parent::closeConnection();
+        }
+    }
+    
+
+    public function delete($id){
+        $connection = parent::openConnection();
+        try {
+            $stmt = $connection->prepare("DELETE FROM deductions WHERE deduction_id = ? ");
+            $stmt->bindParam(1, $id, PDO::PARAM_INT);
+            $stmt->execute();
+            return 200;
+        } catch (PDOException $e) {
+            error_log("Error: " . $e->getMessage());
+        } finally {
+            parent::closeConnection();  
+        }
+    }
+
+}
---
+++ src/services/deductionApi.php
@@ -0,0 +1,21 @@
+<?php 
+require_once '../../config/database.php';
+
+class DeductionApi extends Database {
+
+    public function getAll() {
+        $connection = parent::openConnection();
+        try {
+            $stmt = $connection->prepare("SELECT * FROM deductions");
+            $stmt->execute();
+            $data = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+            return $data;
+        } catch (PDOException $e) {
+            $connection->rollBack();
+            error_log("Error: " . $e->getMessage());
+        } finally {
+            parent::closeConnection();  
+        }
+    }
+
+}

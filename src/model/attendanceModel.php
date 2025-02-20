65f18964a52ddbc890ba8bffe72376340bbefd98
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-02-20 23:39:46 +0800

    added overtime

--- src/model/attendanceModel.php
+++ src/model/attendanceModel.php
@@ -46,30 +46,40 @@ class AttendanceModel extends Database {
             $totalMinutes_2 = $totalHours_2 * 60;
             // TOTAL MINUTES 
             $minutes_worked = $totalMinutes_1 + $totalMinutes_2;
+            if($minutes_worked > 480){
+                $overtime = $minutes_worked - 480;
+                $regular_hr = $minutes_worked - $overtime;
+            }else{
+                $overtime = 0; 
+                $regular_hr =  $minutes_worked;
+            }
 
 
             // ATTENDANCES
             $attendance = $connection->prepare("INSERT INTO attendances 
-                        (employee_id,time_in_1,time_out_1,time_in_2,time_out_2,attendance_status,total_worked_minutes,attendance_date,total_late_minutes) 
-                        VALUES (?,?,?,?,?,?,?,?,?)");
+                        (employee_id,time_in_1,time_out_1,time_in_2,time_out_2,attendance_status,total_worked_minutes,attendance_date,total_late_minutes,Overtime) 
+                        VALUES (?,?,?,?,?,?,?,?,?,?)");
             $attendance->bindParam(1,$data['employee_id']);
             $attendance->bindParam(2,$data['time_in_1']);
             $attendance->bindParam(3,$data['time_out_1']);
             $attendance->bindParam(4,$data['time_in_2']);
             $attendance->bindParam(5,$data['time_out_2']);
             $attendance->bindParam(6,$status);
-            $attendance->bindParam(7,$minutes_worked);
+            $attendance->bindParam(7,$regular_hr);
             $attendance->bindParam(8,$data['attendance_date']);
             $attendance->bindParam(9,$late_minutes);
+            $attendance->bindParam(10,$overtime);
             $attendance->execute();
 
-            $daily_wage = $minutes_worked * ($employee_schedule[0]['hourly_rate'] / 60);
+            $daily_wage = $regular_hr * ($employee_schedule[0]['hourly_rate'] / 60);
+            $overtime_pay = $overtime * (($employee_schedule[0]['hourly_rate'] / 60) * 1.3);
             $earning = $connection->prepare("INSERT INTO earnings 
-            (employee_id,daily_wages,earning_date) 
-            VALUES (?,?,?)");
+            (employee_id,daily_wages,earning_date,Overtime_pay) 
+            VALUES (?,?,?,?)");
             $earning->bindParam(1,$data['employee_id']);
             $earning->bindParam(2,$daily_wage);
             $earning->bindParam(3,$data['attendance_date']);
+            $earning->bindParam(4,$overtime_pay);
             $earning->execute();
 
             $connection->commit();

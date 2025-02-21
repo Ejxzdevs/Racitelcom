e294bf481f9b33b8493cee38af8776a7676ccb99
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-02-21 15:43:00 +0800

    add query for emp allowances & deductions

--- src/model/payrollModel.php
+++ src/model/payrollModel.php
@@ -7,28 +7,65 @@ class PayrollModel extends Database {
         $connection = parent::openConnection();
         try {
             $connection->beginTransaction();
+    
+            // Insert into payrolls
             $stmt = $connection->prepare("INSERT INTO payrolls 
-                        (payroll_type,start_date,end_date,deduction_status,pay_date) 
-                        VALUES (?,?,?,?,?)");
+                        (payroll_type, start_date, end_date, pay_date) 
+                        VALUES (?, ?, ?, ?)");
             $stmt->bindParam(1, $data['payroll_type']);
             $stmt->bindParam(2, $data['start_date']);
             $stmt->bindParam(3, $data['end_date']);
-            $stmt->bindParam(4, $data['deduction_status']);
-            $stmt->bindParam(5, $data['pay_date']);
+            $stmt->bindParam(4, $data['pay_date']);
             $stmt->execute();
+    
+            // Get the last inserted ID
+            $lastInsertId = $connection->lastInsertId();
+    
+            // Fetch allowances
+            $getAllowance = $connection->prepare("SELECT allowance_id,employee_id 
+                                                  FROM emp_allowances 
+                                                  WHERE is_deleted = 0 
+                                                  AND emp_allowance_status = 'ON'");
+            $getAllowance->execute();
+            $allowances = $getAllowance->fetchAll(PDO::FETCH_ASSOC);
+    
+            // Insert into emp_payroll_allowances for each allowance of employees
+            $insertAllowances = $connection->prepare('INSERT INTO emp_payroll_allowances 
+                                                      (payroll_id, employee_id, allowance_id) 
+                                                      VALUES (?, ?, ?)');
+    
+            // Loop through each allowance and insert
+            foreach ($allowances as $allowance) {
+                $insertAllowances->bindParam(1, $lastInsertId);
+                $insertAllowances->bindParam(2, $allowance['employee_id']);
+                $insertAllowances->bindParam(3, $allowance['allowance_id']);
+                $insertAllowances->execute();
+            }
 
-            $getAttendance = $connection->prepare('SELECT * FROM attendances
-                INNER JOIN employees on employees.employee_id = attendances.employee_id 
-                WHERE attendances.attendance_date BETWEEN ? AND ?
-            ');
-            $stmt->bindParam(1, $data['start_date']);
-            $stmt->bindParam(2, $data['end_date']);
-            $getAttendance->execute();
-            $employee_attendance = $getAttendance->fetchAll(PDO::FETCH_ASSOC); 
-            print_r($employee_attendance);
+             // Fetch allowances
+             $getDeduction= $connection->prepare("SELECT deduction_id,employee_id 
+                                                FROM emp_deductions 
+                                                WHERE is_deleted = 0 
+                                                AND emp_deduction_status = 'ON'");
+             $getDeduction->execute();
+             $deductions = $getDeduction->fetchAll(PDO::FETCH_ASSOC);
 
+            // Insert into emp_payroll_deduction for each deduction of employees
+            $insertdeduction = $connection->prepare('INSERT INTO emp_payroll_deductions 
+                 (payroll_id, employee_id, deduction_id) 
+                 VALUES (?, ?, ?)');
+
+            foreach ($deductions as $deduction) {
+                        $insertdeduction->bindParam(1, $lastInsertId);
+                        $insertdeduction->bindParam(2, $deduction['employee_id']);
+                        $insertdeduction->bindParam(3, $deduction['deduction_id']);
+                        $insertdeduction->execute();
+            }
+
+    
+            // Commit transaction
             $connection->commit();
-            // return 200;
+            return 200;
         } catch (PDOException $e) {
             $connection->rollBack();
             echo "Error: " . $e->getMessage();
@@ -37,6 +74,7 @@ class PayrollModel extends Database {
             parent::closeConnection();
         }
     }
+    
 
     
     public function update($data) {

86c0ca23b2dd5828780479d6897e32c15c301f58
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-02-21 15:42:26 +0800

    fix bug

--- src/controller/payrollController.php
+++ src/controller/payrollController.php
@@ -38,11 +38,10 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     try {
         if (isset($_POST['insert'])) {
             $status = $payroll->create($_POST);
-            // echo $status === 200 
-            //     ? "<script>alert('Data Added Successfully')</script><script>window.location.href='../view/main.php?route=payroll'</script>"
-            //     : "<script>alert('Data insertion failed')</script><script>window.location.href='../view/main.php?route=payroll'</script>";
+            echo $status === 200 
+                ? "<script>alert('Data Added Successfully')</script><script>window.location.href='../view/main.php?route=payroll'</script>"
+                : "<script>alert('Data insertion failed')</script><script>window.location.href='../view/main.php?route=payroll'</script>";
         }
-
         if (isset($_POST['update'])) {
             
            $status = $payroll->update($_POST);

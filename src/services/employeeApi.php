de8553f4b449757f7b6c098dbcfdd50ab698609e
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-03-08 22:54:13 +0800

    add SQL query to  retrive all employees based on date hired

--- src/services/employeeApi.php
+++ src/services/employeeApi.php
@@ -24,4 +24,26 @@ class EmployeeApi extends Database {
         }
     }
 
+    public function getAllReport($date) {
+        $connection = parent::openConnection();
+        try {
+            $stmt = $connection->prepare(
+                "SELECT * FROM employees 
+                INNER JOIN departments ON employees.department_id = departments.department_id
+                INNER JOIN positions ON employees.position_id = positions.position_id
+                INNER JOIN schedules ON employees.schedule_id = schedules.schedule_id
+                WHERE date_hired <= :date"
+            );
+            $stmt->bindParam(":date", $date);
+            $stmt->execute();
+            $data = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+            return $data;
+        } catch (PDOException $e) {
+            $connection->rollBack();
+            error_log("Error: " . $e->getMessage());
+        } finally {
+            parent::closeConnection();  
+        }
+    }
+
 }

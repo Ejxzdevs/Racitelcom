e754672d0813eec7330b608611754140fd4a00e5
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-03-19 22:59:33 +0800

    feat: add method to fetch reports for the current month

--- src/services/reportApi.php
+++ src/services/reportApi.php
@@ -17,4 +17,24 @@ class ReportApi extends Database {
         }
     }
 
+    public function getReportsForCurrentMonth() {
+        $connection = parent::openConnection();
+        try {
+            $stmt = $connection->prepare("
+                SELECT * FROM reports 
+                    WHERE is_deleted = 0 
+                    AND created_at BETWEEN DATE_FORMAT(CURRENT_DATE, '%Y-%m-01') 
+                    AND CONCAT(CURRENT_DATE, ' 23:59:59')
+            ");
+            $stmt->execute();
+            $data = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+            return $data;
+        } catch (PDOException $e) {
+            error_log("Error: " . $e->getMessage());
+            return [];
+        } finally {
+            parent::closeConnection();  
+        }
+    }
+
 }

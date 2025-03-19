560618ba4d745f4cf4ffaeadd736a3ac084da34a
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-03-20 04:31:23 +0800

    style(ui): add cursor-pointer to login btn

--- src/view/login.php
+++ src/view/login.php
@@ -28,7 +28,7 @@
                 </div>
             </div>
             <div class="flex justify-center">
-                <button type="submit" class="w-full py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">Login</button>
+                <button type="submit" class="w-full py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer">Login</button>
             </div>
         </form>
     </div>

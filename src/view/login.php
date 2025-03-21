d9d2b5261b4074df2a5b1e21a8b390298fb55762
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-03-21 13:43:59 +0800

    style: Enhance UI design of login page

--- src/view/login.php
+++ src/view/login.php
@@ -8,27 +8,27 @@
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
     <title>User Login</title>
 </head>
-<body class="bg-gray-800 h-screen flex justify-center items-center">
-    <div class="w-full max-w-md bg-white p-8 rounded-lg shadow-lg">
-        <h2 class="text-2xl font-semibold text-center text-gray-700 mb-6">User Login</h2>
+<body class="bg-[url('../../src/assets/images/bg/bgraci.jpg')] bg-cover bg-center h-screen bg-gray-800 h-screen flex justify-center items-center">
+    <div class="w-full max-w-md backdrop-blur-sm text-white p-8 rounded-lg shadow-lg border border-gray-300">
+        <h2 class="text-2xl font-semibold text-center mb-6">User Login</h2>
         <form action="../controller/userController.php" method="post">
             <div class="mb-4 relative">
-                <label for="edit_number" class="block text-sm font-medium text-gray-700">Email</label>
+                <label for="edit_number" class="block text-sm font-medium mb-2">Email</label>
                 <div class="flex items-center border border-gray-300 rounded-md">
                     <i class="fas fa-envelope text-gray-500 pl-3"></i>
-                    <input type="text" name="email" class="text-sm p-2 w-full border-none focus:outline-none focus:ring-2 focus:ring-blue-500 pl-2" placeholder="Enter Email:" required>
+                    <input type="text" name="email" class="text-sm p-2 w-full border-none focus:outline-none focus:ring-1 focus:ring-blue-300 pl-2" placeholder="Enter Email:" required>
                     <input type="hidden" name="login" required>
                 </div>
             </div>
             <div class="mb-4 relative">
-                <label for="edit_address" class="block text-sm font-medium text-gray-700">Password</label>
+                <label for="edit_address" class="block text-sm font-medium mb-2">Password</label>
                 <div class="flex items-center border border-gray-300 rounded-md">
                     <i class="fas fa-lock text-gray-500 pl-3"></i>
-                    <input type="password"  name="password" class="text-sm p-2 w-full border-none focus:outline-none focus:ring-2 focus:ring-blue-500 pl-2" placeholder="Enter Password:" required>
+                    <input type="password"  name="password" class="text-sm p-2 w-full border-none focus:outline-none focus:ring-1 focus:ring-blue-300 pl-2" placeholder="Enter Password:" required>
                 </div>
             </div>
             <div class="flex justify-center">
-                <button type="submit" class="w-full py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer">Login</button>
+            <button type="submit" class="w-full py-2 bg-white text-gray-800 font-semibold rounded-md hover:bg-blue-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer transition-all duration-200 ease-in-out">Login</button>
             </div>
         </form>
     </div>
